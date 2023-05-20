271dbfa56c546deeecab402d80de30c956596527
João Santos <jm_santos7@hotmail.com>
2023-05-20 15:59:02 +0200

    Change output to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    assets: true
+    assets: true,
+    hybridOutput: true,
   },
   integrations: [mdx()],
-  output: "server",
-  adapter: vercel()
+  output: 'hybrid',
+  adapter: vercel(),
 });
